cc09935736fa2e6a3040e0156f3cae1059e17154
martindufresne114 <129678304+martindufresne114@users.noreply.github.com>
2023-04-02 11:38:11 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+let Dé1 = 0
+let Dé2 = 0
+let Mise = 0
+let Somme = 0
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+    	
+    }
+    basic.showString("Mise?")
+})
+input.onButtonPressed(Button.AB, function () {
+    Dé1 = 0
+    Dé2 = 0
+    Mise = 0
+    Somme = 0
+    basic.showString("Debut")
+})
+input.onButtonPressed(Button.B, function () {
+    Mise += 1
+})
