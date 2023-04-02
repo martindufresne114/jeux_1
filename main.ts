7af939d0e2c00dc636dc6055c131c92e196d3766
martindufresne114 <129678304+martindufresne114@users.noreply.github.com>
2023-04-02 13:20:46 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,60 @@
+let Mise = 0
+let Prédiction = 0
 let Dé1 = 0
 let Dé2 = 0
-let Mise = 0
 let Somme = 0
 input.onButtonPressed(Button.A, function () {
-    if (true) {
-    	
-    }
-    basic.showString("Mise?")
+    Mise += 1
+    basic.showNumber(Mise)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    Mise = 0
+    Prédiction = 0
     Dé1 = 0
     Dé2 = 0
-    Mise = 0
     Somme = 0
     basic.showString("Debut")
+    basic.showString("Mise (A)?")
+    basic.showString("Prediction (B)?")
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Mise == 0) {
+        basic.showIcon(IconNames.Sad)
+    }
+    if (Prédiction == 0) {
+        basic.showIcon(IconNames.Angry)
+    }
+    if (Dé1 == 0) {
+        basic.showIcon(IconNames.Sad)
+    }
+    if (Dé1 == 0) {
+        basic.showIcon(IconNames.Angry)
+    }
+    if (Dé2) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        Somme = Dé1 + Dé2
+        if (Prédiction > Somme) {
+            basic.showString("Tu perds TOUT")
+        } else if (Prédiction < Somme) {
+            basic.showString("Récupère ta mise")
+        } else {
+            basic.showString("Gros lot !")
+        }
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    Mise += 1
+    Prédiction += 1
+    basic.showNumber(Prédiction)
+})
+input.onGesture(Gesture.Shake, function () {
+    if (Dé1 == 0) {
+        Dé1 = randint(1, 6)
+        basic.showString("D1: " + Dé1)
+    } else if (Dé2 == 0) {
+        Dé2 = randint(1, 6)
+        basic.showString("D2: " + Dé2)
+    } else {
+        basic.showString("Fini (A+B)")
+    }
 })
